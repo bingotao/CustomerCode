4b075470628a67c8edd39e994983cbf93fb2f32c
韬 陈 <444034939@qq.com>
2016-06-02 10:54:27 +0800

    图片压缩及转换

---
+++ CustomerCode/CustomerCommon/Image/ImageConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingotao.Customer.BaseLib
+{
+    public class ImageConverter
+    {
+        /// <summary> 
+        /// 字节流转换成图片 
+        /// </summary> 
+        /// <param name="byt">要转换的字节流</param> 
+        /// <returns>转换得到的Image对象</returns> 
+        public static Image BytToImg(byte[] byt)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream(byt);
+                Image img = Image.FromStream(ms);
+                return img;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        ///  图片转换成字节流 
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public static byte[] ImageToByteArray(Image image)
+        {
+            ImageFormat format = image.RawFormat;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                byte[] buffer = new byte[ms.Length];
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.Read(buffer, 0, buffer.Length);
+                return buffer;
+            }
+        }
+
+    }
+}
