c24794e36a32528c71ac40c7b63328e75be81b9a
韬 陈 <444034939@qq.com>
2016-06-22 14:48:57 +0800

    地类分析

--- CustomerCode/ConsoleTest/Program.cs
+++ CustomerCode/ConsoleTest/Program.cs
@@ -6,6 +6,7 @@ using ESRI.ArcGIS.Geodatabase;
 using ESRI.ArcGIS.Geometry;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,18 +15,116 @@ namespace ConsoleTest
 {
     class Program
     {
+        class KeyValue
+        {
+            public string Key;
+            public double Value;
+        }
         static void Main(string[] args)
         {
+
             new LicenseInitializer().InitLicense();
-            IFeatureWorkspace wk = WorkspaceUtilities.GetWorkspace(@"C:\Data\浙江省工业用地数据\富阳数据\fy_gyyd.gdb", enumWorkspaceType.GDB) as IFeatureWorkspace;
-            IFeatureClass test = wk.OpenFeatureClass("TEST");
-            IFeatureClass dltb = wk.OpenFeatureClass("DLTB");
+            //IFeatureWorkspace wk = WorkspaceUtilities.GetWorkspace(@"C:\Data\浙江省工业用地数据\富阳数据\fy_gyyd.gdb", enumWorkspaceType.GDB) as IFeatureWorkspace;
+            //IFeatureClass test = wk.OpenFeatureClass("TEST");
+            //IFeatureClass dltb = wk.OpenFeatureClass("DLTB");
 
-            IFeature ftTest = test.GetFeature(1);
+            //IFeature ftTest = test.GetFeature(1);
 
-            var d = CommonAnalysis.Intersect(ftTest.ShapeCopy as IPolygon, dltb, "DLBM");
+            //var d = CommonAnalysis.Intersect(ftTest.ShapeCopy as IPolygon, dltb, "DLBM");
+
+
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[] { new DataColumn("DLBM"), new DataColumn("QSXZ"), new DataColumn("MJ", typeof(double)) });
+
+            dt.LoadDataRow(new object[] { "011", "30", 100 }, true);
+            dt.LoadDataRow(new object[] { "011", "31", 220 }, true);
+            dt.LoadDataRow(new object[] { "011", "10", 80 }, true);
+            dt.LoadDataRow(new object[] { "012", "10", 64 }, true);
+            dt.LoadDataRow(new object[] { "012", "20", 32 }, true);
+            dt.LoadDataRow(new object[] { "206", "10", 55 }, true);
+            dt.LoadDataRow(new object[] { "203", "10", 55 }, true);
+
+            DataColumn dcQSXZ2 = new DataColumn("QSXZ2");
+            dcQSXZ2.Expression = "iif(QSXZ in('10','20'),'G','J')";
+            dt.Columns.Add(dcQSXZ2);
+
+            DataColumn dcSDL = new DataColumn("SDL");
+            dcSDL.Expression = "iif(DLBM in('011','012','013','021','022','023','031','032','033','041','042','104','114','117','122','123'),'N',iif(DLBM in ('201','202','203','204','205','101','102','105','106','107','113','117','118'),'J','W'))";
+            dt.Columns.Add(dcSDL);
+            //按地类、按权属汇总
+            var dlbm_qsxz = from c in dt.AsEnumerable()
+                            group c by new { SDL = c.Field<string>("SDL"), DLBM = c.Field<string>("DLBM"), QSXZ = c.Field<string>("QSXZ2") } into g
+
+                            select new
+                            {
+                                SDL = g.Key.SDL,
+                                DLBM = g.Key.DLBM,
+                                QSXZ = g.Key.QSXZ,
+                                MJ = g.Sum(p => p.Field<double>("MJ"))
+                            };
+            var dlbm_qsxzList = (from c in dlbm_qsxz
+                                 select new KeyValue
+                                 {
+                                     Key = string.Format("{0}_{1}", c.DLBM, c.QSXZ),
+                                     Value = c.MJ
+                                 }
+                                 ).ToList();
+
+            //按地类汇总
+            var dlbm = from c in dlbm_qsxz
+                       group c by c.DLBM into g
+                       select new KeyValue
+                       {
+                           Key = string.Format("{0}_Z", g.Key),
+                           Value = g.Sum(p => p.MJ)
+                       };
+            var dlbmList = dlbm.ToList();
+
+            //按三大类、按权属汇总
+            var sdl_qsxz = from c in dlbm_qsxz
+                           group c by new { SDL = c.SDL, QSXZ = c.QSXZ } into g
+                           select new KeyValue
+                           {
+                               Key = string.Format("{0}_{1}", g.Key.SDL, g.Key.QSXZ),
+                               Value = g.Sum(p => p.MJ)
+                           };
+            var sdl_qsxzList = sdl_qsxz.ToList();
+
+            //按三大类
+            var sdl = from c in dlbm_qsxz
+                      group c by new { SDL = c.SDL } into g
+                      select new KeyValue
+                      {
+                          Key = string.Format("{0}_Z", g.Key.SDL),
+                          Value = g.Sum(p => p.MJ)
+                      };
+            var sdlList = sdl.ToList();
+
+            List<KeyValue> list = new List<KeyValue>();
+
+            //全部、分权属汇总
+            var z_qsxz = from c in dlbm_qsxz
+                         group c by c.QSXZ into g
+                         select new KeyValue
+                         {
+                             Key = string.Format("Z_{0}", g.Key),
+                             Value = g.Sum(p => p.MJ)
+                         };
+            var z_qsxzList = z_qsxz.ToList();
+            //全部汇总
+            var z = new KeyValue
+            {
+                Key = string.Format("Z_Z"),
+                Value = dlbm_qsxz.Sum(p => p.MJ)
+            };
+
+            list.AddRange(sdlList);
+            list.AddRange(sdl_qsxzList);
+            list.AddRange(dlbmList);
+            list.AddRange(dlbm_qsxzList);
+            list.AddRange(z_qsxzList);
+            list.Add(z);
+
         }
-
-
     }
 }
--- CustomerCode/CustomerGIS/Analysis/CommonAnalysis.cs
+++ CustomerCode/CustomerGIS/Analysis/CommonAnalysis.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bingotao.Customer.GIS.Feature;
+using System.Data;
 
 namespace Bingotao.Customer.GIS.Analysis
 {
@@ -19,7 +20,7 @@ namespace Bingotao.Customer.GIS.Analysis
         /// <param name="ftCls">所要分析的要素类</param>
         /// <param name="typeField">分析的字段</param>
         /// <returns></returns>
-        public static Dictionary<string, double> Intersect(IPolygon polygon, IFeatureClass ftCls, params string[] typeFields)
+        public static Dictionary<string, double> Intersect2(IPolygon polygon, IFeatureClass ftCls, params string[] typeFields)
         {
             Dictionary<string, double> result = new Dictionary<string, double>();
             IFeatureCursor ftCursor = null;
@@ -61,6 +62,61 @@ namespace Bingotao.Customer.GIS.Analysis
                 Releaser.AEComReleaser.Release(ftCursor, ft);
             }
             return result;
+        }
+
+        /// <summary>
+        /// 相交分析
+        /// </summary>
+        /// <param name="polygon">面</param>
+        /// <param name="ftCls">所要分析的要素类</param>
+        /// <param name="typeField">分析的字段</param>
+        /// <returns></returns>
+        public static DataTable Intersect(IPolygon polygon, IFeatureClass ftCls, params string[] typeFields)
+        {
+            DataTable dtResult = new DataTable();
+            IFeatureCursor ftCursor = null;
+            IFeature ft = null;
+            try
+            {
+                (polygon as ITopologicalOperator).Simplify();
+
+                Dictionary<string, int> fieldNameAndIndex = new Dictionary<string, int>();
+                foreach (string fieldName in typeFields)
+                {
+                    fieldNameAndIndex.Add(fieldName, FeatureUtilities.GetFieldIndex(ftCls, fieldName));
+                    dtResult.Columns.Add(new DataColumn(fieldName));
+                }
+                dtResult.Columns.Add(new DataColumn("MJ", typeof(double)));
+
+                ISpatialFilter spatialFilter = new SpatialFilterClass();
+                spatialFilter.Geometry = polygon;
+                spatialFilter.SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects;
+                ftCursor = ftCls.Search(spatialFilter, true);
+                while ((ft = ftCursor.NextFeature()) != null)
+                {
+                    string type = string.Empty;
+                    IGeometry geo = ft.Shape;
+                    IGeometry geoIntersect = (geo as ITopologicalOperator).Intersect(polygon, esriGeometryDimension.esriGeometry2Dimension);
+                    DataRow drNew = dtResult.NewRow();
+                    double intersectArea = (geoIntersect as IArea).Area;
+                    drNew["MJ"] = intersectArea;
+                    foreach (string key in fieldNameAndIndex.Keys)
+                    {
+                        string value = ft.Value[fieldNameAndIndex[key]].ToString();
+                        drNew[key] = value;
+                    }
+                    dtResult.Rows.Add(drNew);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Releaser.AEComReleaser.Release(ftCursor, ft);
+            }
+            return dtResult;
         }
     }
 }
